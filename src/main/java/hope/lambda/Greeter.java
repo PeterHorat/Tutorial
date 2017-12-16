cb5326a8507d82a99b591b374f5e8e6a64cf0f89
PeterHorat <peterhorat@hispeed.ch>
2017-12-16 17:02:58 +0100

    Initial commit

---
+++ src/main/java/hope/lambda/Greeter.java
@@ -0,0 +1,12 @@
+package hope.lambda;
+
+public class Greeter {
+    public void greet() {
+        System.out.print("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet();
+    }
+}
